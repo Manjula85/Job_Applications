7b699f141c814840a38966000ef819b82b5ca81b
Manjula Guneratne <uftcodingbootcamp@gmail.com>
2021-07-29 16:40:04 -0400

    css format adjusted a bit and NaN error fixed for job_position

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var buttonEl = document.querySelector("#add-job");
 
 //Data collected from the Modal
 var date_posted = document.querySelector("#date_posted");
-var position = document.querySelector("#position");
+var job_position = document.querySelector("#job_position");
 var job_link = document.querySelector("#job_link");
 
 var createJobHandler = function () {
@@ -11,8 +11,8 @@ var createJobHandler = function () {
   jobItemEl.className = "job-item";
 
   var jobInfoEl = document.createElement("div");
-  jobInfoEl.innerHTML = "<li class='job-item'> Date posted: " + date_posted.value +"<br /> Job position: " + 
-  + position.value + "<br /> Job link: "+ job_link.value +"</li>";
+  jobInfoEl.innerHTML = "<li class='job-item'> Date posted: " + date_posted.value +"<br /> Job position: " 
+  + job_position.value + "<br /> Job link: " + job_link.value + "</li>";
   
   jobItemEl.appendChild(jobInfoEl);
   // Add the entire item to the list
