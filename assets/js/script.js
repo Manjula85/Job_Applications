06d020753e0c7c681df42c134e8c7b69f59ae391
Manjula Guneratne <uftcodingbootcamp@gmail.com>
2021-07-29 09:10:49 -0400

    added a function to complete DOM execution

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
-var jobsToApplyEl = document.querySelector('#jobs-in-review');
-var buttonEl = document.querySelector('#save-job');
+var jobsToApplyEl = document.querySelector("#jobs-in-review");
+var buttonEl = document.querySelector("#save-job");
 
-buttonEl.addEventListener('click', function () {
-    var jobItemEl = document.createElement('li');
-    jobItemEl.textContent = "hello";
-    jobItemEl.className = 'job-item';
-    jobsToApplyEl.appendChild(jobItemEl);
-});
+var createJobHandler = function () {
+  var jobItemEl = document.createElement("li");
+  jobItemEl.textContent = "hello";
+  jobItemEl.className = "job-item";
+  jobsToApplyEl.appendChild(jobItemEl);
+};
+
+buttonEl.addEventListener("click", createJobHandler);
