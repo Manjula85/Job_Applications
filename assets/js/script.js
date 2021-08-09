d6be429184364f122fb1f3543ce71706ca2ae5ca
Manjula Guneratne <uftcodingbootcamp@gmail.com>
2021-08-09 16:54:57 -0400

    object added to collect inputted data in one location

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,16 +12,20 @@ var date_posted = document.querySelector("#date_posted");
 var job_position = document.querySelector("#job_position");
 var job_link = document.querySelector("#job_link");
 
-var completeEditJob = function (date_posted, job_position, job_link, jobId) {
+//Data storage
+var jobs = [];
+
+var completeEditJob = function (jobDataObj, jobId) {
   //find the matching task list item
   var jobSelected = document.querySelector(
     ".job-item[data-job-id='" + jobId + "']"
   );
 
   //set new values
-  jobSelected.querySelector("#date").textContent = date_posted.value;
-  jobSelected.querySelector("#position").textContent = job_position.value;
-  jobSelected.querySelector("#link").textContent = job_link.value;
+  jobSelected.querySelector("#date").textContent = jobDataObj.date_posted.value;
+  jobSelected.querySelector("#position").textContent =
+    jobDataObj.job_position.value;
+  jobSelected.querySelector("#link").textContent = jobDataObj.job_link.value;
 
   formEl.removeAttribute("data-job-id");
   document.querySelector("#add-job").textContent = "Save";
@@ -30,6 +34,18 @@ var completeEditJob = function (date_posted, job_position, job_link, jobId) {
 var createJobHandler = function () {
   var jobItemEl = document.createElement("li");
   jobItemEl.className = "job-item";
+
+  //setting the collected data to an Obj
+  var jobDataObj = {
+    date_posted: date_posted,
+    job_position: job_position,
+    job_link: job_link,
+    status: "In review",
+  };
+  
+  // testing
+  console.log(jobDataObj);
+  console.dir(jobDataObj.status);
 
   //adding draggable
   jobItemEl.setAttribute("draggable", "true");
@@ -40,7 +56,7 @@ var createJobHandler = function () {
   //has data attribute, so get job id and call func() to complete edit process
   if (isEdit) {
     var jobId = formEl.getAttribute("data-job-id");
-    completeEditJob(date_posted, job_position, job_link, jobId);
+    completeEditJob(jobDataObj, jobId);
   }
   //no data attribute, so create object as normal and pass data
   else {
@@ -51,15 +67,15 @@ var createJobHandler = function () {
     jobInfoEl.innerHTML =
       "<li class='job-item'> Date posted: " +
       "<span id='date'>" +
-      date_posted.value +
+      jobDataObj.date_posted.value +
       "</span>" +
       "<br /> Job position: " +
       "<span id='position'>" +
-      job_position.value +
+      jobDataObj.job_position.value +
       "</span>" +
       "<br /> Job link: " +
       "<span id='link'>" +
-      job_link.value +
+      jobDataObj.job_link.value +
       "</span>" +
       "</li>";
 
@@ -144,13 +160,13 @@ var editJob = function (jobId) {
 
   //get content from posted date, job link and job position
   var datePostedModal = jobSelected.querySelector("#date").textContent;
-  date_posted.value = datePostedModal;
+  jobDataObj.date_posted.value = datePostedModal;
 
   var jobPositionModal = jobSelected.querySelector("#position").textContent;
-  job_position.value = jobPositionModal;
+  jobDataObj.job_position.value = jobPositionModal;
 
   var jobLinkModal = jobSelected.querySelector("#link").textContent;
-  job_link.value = jobLinkModal;
+  jobDataObj.job_link.value = jobLinkModal;
 
   document.querySelector("#add-job").textContent = "Edit post";
 };
@@ -209,34 +225,38 @@ var dragJobHandler = function (event) {
 
   //to verify the data was stored
   var getId = event.dataTransfer.getData("text/plain");
-  console.log("getId: ", getId, typeof getId);
 };
 
 var dropZoneDragHandler = function (event) {
   var jobListEl = event.target.closest(".job-list");
   if (jobListEl) {
     event.preventDefault();
-    
-    jobListEl.setAttribute("style", "background: #ffb54f; border-style: dashed;");
+
+    jobListEl.setAttribute(
+      "style",
+      "background: #ffb54f; border-style: dashed;"
+    );
   }
 };
 
-var dropJobHandler = function(event){
+var dropJobHandler = function (event) {
   var id = event.dataTransfer.getData("text/plain");
-  var draggableElement = document.querySelector("[data-job-id='"+id+"']");
+  var draggableElement = document.querySelector("[data-job-id='" + id + "']");
 
   var dropZoneEl = event.target.closest(".job-list");
   var statusType = dropZoneEl.id;
 
-  var statusSelectEl = draggableElement.querySelector("select[name='status-change']");
+  var statusSelectEl = draggableElement.querySelector(
+    "select[name='status-change']"
+  );
 
-  if(statusType === "In review"){
+  if (statusType === "In review") {
     statusSelectEl.selectedIndex = 0;
-  } else  if(statusType === "Jobs to Apply to"){
+  } else if (statusType === "Jobs to Apply to") {
     statusSelectEl.selectedIndex = 1;
-  } else if(statusType === "Already applied to"){
+  } else if (statusType === "Already applied to") {
     statusSelectEl.selectedIndex = 2;
-  };
+  }
 
   //remove the drag colouring
   dropZoneEl.removeAttribute("style");
@@ -244,9 +264,9 @@ var dropJobHandler = function(event){
   dropZoneEl.appendChild(draggableElement);
 };
 
-var dragLeaveHandler = function (event){
+var dragLeaveHandler = function (event) {
   var jobListEl = event.target.closest(".job-list");
-  if (jobListEl){
+  if (jobListEl) {
     jobListEl.removeAttribute("style");
   }
 };
